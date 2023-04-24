1af9ee53ed01f8592b3bf989b2bb5791429f7778
tttamaki1 <130736956+tttamaki1@users.noreply.github.com>
2023-04-24 19:11:37 +0900

    commit6

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,7 @@ class CommentsController < ApplicationController
     else
       @prototype = @comment.prototype
       @comments = @prototype.comments
-      redirect_to prototypes(@comment.prototype) 
+      render "prototypes/show"
     end
   end
 
